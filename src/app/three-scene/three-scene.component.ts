eed6d5ef2f1106d14cca58cd29f650ba92d2b2ab
Anurag <anurag.131092@gmail.com>
2020-10-04 23:17:27 +0530

    Added billboards correctly

--- src/app/three-scene/three-scene.component.ts
+++ src/app/three-scene/three-scene.component.ts
@@ -238,13 +238,13 @@ export class ThreeSceneComponent implements OnInit {
   introduce(obj){
     const { scene } = this, total = 6
     scene.add(obj)
-    this.count++
     
-    l(`${this.count} of ${total} items added : ${obj.name}`)
-    l(
-      `Loading ${Math.round(this.count*100/total)}%`
-    );
-    (this.count === total) && l("All items added!")    
+    // this.count++
+    // l(`${this.count} of ${total} items added : ${obj.name}`)
+    // l(
+    //   `Loading ${Math.round(this.count*100/total)}%`
+    // );
+    // (this.count === total) && l("All items added!")    
   }  
 
   introduceCSS3D(obj){ this.sceneCSS.add(obj) }
@@ -326,7 +326,7 @@ export class ThreeSceneComponent implements OnInit {
         })
       })
     }
-    , addBillboards = () => {
+    , addBuildingAds = () => {
       // create the plane mesh
       var material = new THREE.MeshBasicMaterial({ 
         color: 0x000000, transparent: true, opacity: 0,
@@ -399,143 +399,195 @@ export class ThreeSceneComponent implements OnInit {
       cssObject4.scale.multiplyScalar(.15)
       this.introduceCSS3D(cssObject4)
     }
-    , addBillboards2 = () => {
-      // gltf.load('assets/models/billboards/b1/scene.gltf', obj => { 
-      //   // l(obj)
-      //   const billboard1 = obj.scene 
-      //   billboard1.name = "Billboard 1"
-      //   billboard1.traverse(child => {
-      //     if(child.name === "Billboard_Mat_1"){
-      //       l(child)
-      //       (<any>child).material = new THREE.MeshPhongMaterial({
-      //         color: 0xffffff
-      //       })
-      //     }
-      //   })
-      //   // billboard1.position.z = -250
-      //   // router.scale.multiplyScalar(.03)
-      //   // router.rotation.set(0, Math.PI / 2 + .2, 0)
-      //   // router.position.set(-36.54, 27.6, -120)
-      //   this.introduce(billboard1)
-      // })
-      mtl.load("assets/models/billboards/b2-mod/untitled.mtl", materials => {
+    , createBillBoard = ({ name, billboard, plane, css, scaleFactor }) => {
+      // l(name, billboard, plane, css, scaleFactor)
+      // Group to move billboard and ad mesh together
+      const billGr = new THREE.Group()
+      this.introduce(billGr)
+      billGr.name = name
+      billGr.add(billboard.mesh)
+
+      // Mesh to blend with ad gif
+      const adMesh = createMesh(
+        new THREE.PlaneGeometry(),
+        // Essentially this material creates a 'hole' in our webgl scene to peek 
+        // into the CSS scene behind it
+        new THREE.MeshBasicMaterial({ 
+          // wireframe: true,
+          // color: 0xff0000, 
+          transparent: true, opacity: 0,
+          color: 0x000000, blending: THREE.NoBlending 
+        })
+      )
+      adMesh.name = "Plane"
+      billGr.add(adMesh)
+
+      const { scale: sc, pos: ps } = plane
+      // Set scale to fit ad gif
+      adMesh.scale.set(sc[0], sc[1], sc[2])
+      // Set position relative to billboard model
+      adMesh.position.set(ps[0], ps[1], ps[2])
+
+      // transforms for the group
+      const { pos, rot } = billboard
+      billGr.position.set(pos[0], pos[1], pos[2])
+      billGr.rotation.set(rot[0], rot[1], rot[2])
+      billGr.scale.multiplyScalar(scaleFactor)
+      billGr.updateMatrixWorld()
+
+      // Get world coordinates of the ad mesh 
+      const targetPos = new THREE.Vector3()
+      adMesh.getWorldPosition(targetPos)
+
+      // Add ad gif to CSS3D scene
+      const { id, scale, offset } = css
+      const cssObject = new CSS3DObject(document.getElementById(id))
+      this.introduceCSS3D(cssObject)
+      
+      // Copy world coordinates of the ad mesh to the ad gif
+      cssObject.position.copy(targetPos)
+      if(offset){
+        cssObject.position.x+= offset[0]
+        cssObject.position.y+= offset[1]
+        cssObject.position.z+= offset[2]
+      }
+      cssObject.rotation.copy(billGr.rotation)
+      // To fit into the ad mesh
+      cssObject.scale.multiplyScalar(scale)
+      // To scale with the billboard group
+      cssObject.scale.multiplyScalar(scaleFactor)
+
+      return billGr
+    }
+    , addBillboards = () => {
+
+      mtl.load("assets/models/billboards/b1/untitled.mtl", materials => {
         materials.preload()
-        new OBJLoader()
-        .setMaterials( materials )
-        .load( 'assets/models/billboards/b2-mod/untitled.obj', object => {
-          // const stool = object
-          // stool.name = "Stool"
-          // stool.children[0].castShadow = true
-          // stool.scale.multiplyScalar(.2)
-          // stool.rotation.set(-Math.PI / 2, 0, -Math.PI / 2)
-          // stool.position.set(120, 0, -70)
-          // this.introduce(stool)
-          const billGr1 = new THREE.Group()
-          billGr1.name = "Billboard Gr 1"
-          this.introduce(billGr1)
-          const billboard2 = object 
-          billboard2.name = "Billboard 2"
-          billboard2.scale.multiplyScalar(20)
-          billboard2.rotation.y = Math.PI 
-          billGr1.add(billboard2)
-          // this.introduce(billboard2)
+        new OBJLoader().setMaterials(materials)
+        .load("assets/models/billboards/b1/untitled.obj", object => {
+          // Billboard model - normalize size, scale down later
+          const bb = object 
+          bb.name = "Billboard"
+          bb.scale.multiplyScalar(20)
+          bb.rotation.y = Math.PI 
+          
+          const gr = createBillBoard({
+            name: "Billboard Group 1 (Adidas)",
+            billboard: { mesh: bb, pos: [0, 0, 0], rot: [0, 0, 0] },
+            plane: { scale: [160, 65, 0], pos: [0, 148, 8] },
+            css: { scale: .27, id: "bill1" }, scaleFactor: .5
+          })
 
-          var material = new THREE.MeshBasicMaterial({ 
-            transparent: true, 
-            // wireframe: true,
-            // color: 0xff0000, 
-            color: 0x000000, 
-            opacity: 0,
-            blending: THREE.NoBlending
-          });
-    
-          var geometry = new THREE.PlaneGeometry();
-          var planeMesh= new THREE.Mesh( geometry, material );
-          // add it to the WebGL scene
-          // planeMesh.scale.multiplyScalar(150)
-          planeMesh.name = "Banner 1"
-          planeMesh.scale.set(160, 65, 0)
-          // planeMesh.updateMatrix(); 
-          // planeMesh.geometry.applyMatrix4( planeMesh.matrix );
-          // planeMesh.matrix.identity();
-          // planeMesh.scale.set( 1, 1, 1 );
-          planeMesh.position.y = 148
-          planeMesh.position.z = 8
-          this.introduce(planeMesh)
-          billGr1.add(planeMesh)
-          l(planeMesh)
-          // planeMesh.position.z = 10
+          l(gr)
 
-          billGr1.position.x = 100
-          billGr1.rotation.y = Math.PI / 2
-          billGr1.scale.multiplyScalar(.5)
-          billGr1.updateMatrixWorld()
+          const gr2 = createBillBoard({
+            name: "Billboard Group 4 (Converse)",
+            billboard: { mesh: bb.clone(), pos: [0, 100, 0], rot: [0, 0, 0] },
+            plane: { scale: [129, 65, 0], pos: [0, 148, 8] },
+            css: { scale: .27, offset: [-.15, 0, 0], id: "bill4" }, scaleFactor: .5
+          })
 
-          var targetPos = new THREE.Vector3(); 
-          var targetRot = new THREE.Vector3(); 
-          planeMesh.getWorldPosition( targetPos );
-          // planeMesh.getWorldRotation( targetRot );
-          l(targetPos, targetRot)
-          const cssObject = new CSS3DObject(document.getElementById("bill1"))
-          // cssObject.position.copy(planeMesh.position)
-          cssObject.position.copy(targetPos)
-          cssObject.position.x-= .25
-          cssObject.rotation.copy(billGr1.rotation)
-          cssObject.scale.multiplyScalar(.27*.5)
-          // cssObject.updateMatrix(); 
-          // cssObject.geometry.applyMatrix4( cssObject.matrix );
-          // cssObject.matrix.identity();
-          // cssObject.scale.set( 1, 1, 1 );
-          // l(cssObject.scale)
-          this.introduceCSS3D(cssObject)
+          l(gr2)
 
+          const gr3 = createBillBoard({
+            name: "Billboard Group 6 (Nike)",
+            billboard: { mesh: bb.clone(), pos: [100, 140, 0], rot: [0, 0, 0] },
+            plane: { scale: [129, 65, 0], pos: [0, 148, 8] },
+            css: { scale: .28, offset: [-.15, 0, 0], id: "bill6" }, scaleFactor: .5
+          })
+
+          l(gr3)
+
+          const gr4 = createBillBoard({
+            name: "Billboard Group 7 (KFC)",
+            billboard: { mesh: bb.clone(), pos: [-190, 0, 0], rot: [0, 0, 0] },
+            plane: { scale: [129, 65, 0], pos: [0, 148, 8] },
+            css: { scale: .28, offset: [-.15, 0, 0], id: "bill7" }, scaleFactor: .5
+          })
+
+          l(gr4)
         })
       })
-      // gltf.load('assets/models/billboards/b2-mod/scene.glb', obj => { 
-      //   // l(obj)
-      //   const billboard2 = obj.scene 
-      //   billboard2.name = "Billboard 2"
-      //   // billboard2.position.z = 250
-      //   billboard2.scale.multiplyScalar(20)
-      //   // router.rotation.set(0, Math.PI / 2 + .2, 0)
-      //   // router.position.set(-36.54, 27.6, -120)
-      //   this.introduce(billboard2)
-      // })
-
-      // gltf.load('assets/models/billboards/b3/scene.gltf', obj => { 
-      //   // l(obj)
-      //   const billboard3 = obj.scene 
-      //   billboard3.name = "Billboard 3"
-      //   // billboard3.position.z = 250
-      //   billboard3.scale.multiplyScalar(.75)
-      //   // router.rotation.set(0, Math.PI / 2 + .2, 0)
-      //   // router.position.set(-36.54, 27.6, -120)
-      //   this.introduce(billboard3)
-      // })
-
-      // gltf.load('assets/models/billboards/b4/scene.gltf', obj => { 
-      //   // l(obj)
-      //   const billboard4 = obj.scene 
-      //   billboard4.name = "Billboard 4"
-      //   billboard4.position.x = 250
-      //   billboard4.position.y = 150
-      //   billboard4.scale.multiplyScalar(.5)
-      //   // router.rotation.set(0, Math.PI / 2 + .2, 0)
-      //   // router.position.set(-36.54, 27.6, -120)
-      //   this.introduce(billboard4)
-      // })
       
-      // gltf.load('assets/models/billboards/b5/scene.gltf', obj => { 
-      //   // l(obj)
-      //   const billboard5 = obj.scene 
-      //   billboard5.name = "Billboard 5"
-      //   billboard5.position.x = -250
-      //   // billboard4.position.y = 150
-      //   billboard5.scale.multiplyScalar(20)
-      //   // router.rotation.set(0, Math.PI / 2 + .2, 0)
-      //   // router.position.set(-36.54, 27.6, -120)
-      //   this.introduce(billboard5)
-      // })
+      // Double Sided billboard
+      gltf.load('assets/models/billboards/b2/scene.glb', obj => { 
+        // Billboard model - normalize size, scale down later
+        const bb = obj.scene
+        bb.name = "Billboard"
+        bb.scale.multiplyScalar(10)
+        bb.rotation.y = - Math.PI / 2
+
+        const gr = createBillBoard({
+          name: "Billboard Group 2 (Nike)",
+          billboard: { mesh: bb, pos: [100, 0, 0], rot: [0, 0, 0] },
+          plane: { scale: [75, 44, 0], pos: [0, 100, 2] },
+          css: { scale: .05, id: "bill2" }, scaleFactor: 1
+        })
+
+        l(gr)
+
+        const gr2 = createBillBoard({
+          name: "Billboard Group 3 (Coca Cola)",
+          billboard: { mesh: bb.clone(), pos: [-100, 0, 0], rot: [0, 0, 0] },
+          plane: { scale: [80, 42, 0], pos: [0, 100, 2] },
+          css: { scale: .2, offset: [0, -4.8, 0], id: "bill3" }, scaleFactor: .8
+        })
+
+        l(gr2)
+
+        const gr3 = createBillBoard({
+          name: "Billboard Group 5 (North Face)",
+          billboard: { mesh: bb.clone(), pos: [-100, 120, 0], rot: [0, 0, 0] },
+          plane: { scale: [80, 42, 0], pos: [0, 100, 2] },
+          css: { scale: .14, id: "bill5" }, scaleFactor: .8
+        })
+
+        l(gr3)
+      })
+
+      // Auto animated billboard
+      gltf.load('assets/models/billboards/b3/scene.gltf', obj => { 
+        // l(obj)
+        const billboard3 = obj.scene 
+        billboard3.name = "Billboard 3"
+        // billboard3.position.z = 250
+        billboard3.scale.multiplyScalar(.15)
+        // router.rotation.set(0, Math.PI / 2 + .2, 0)
+        billboard3.position.set(200, 200, 0)
+        billboard3.rotation.set(0, -Math.PI / 2, 0)
+        this.introduce(billboard3)
+      })
+
+      mtl.load("assets/models/billboards/b4/untitled.mtl", materials => {
+        materials.preload()
+        new OBJLoader().setMaterials(materials)
+        .load("assets/models/billboards/b4/untitled.obj", object => {
+          // Billboard model - normalize size, scale down later
+          const bb = object 
+          bb.name = "Billboard"
+          bb.scale.multiplyScalar(20)
+          bb.rotation.y = Math.PI 
+          
+          const gr = createBillBoard({
+            name: "Billboard Group 8 (Adidas)",
+            billboard: { mesh: bb, pos: [-200, 100, 0], rot: [0, 0, 0] },
+            plane: { scale: [143, 108, 0], pos: [0, 167.5, 8] },
+            css: { scale: .18, id: "bill8" }, scaleFactor: .5
+          })
+
+          l(gr)
+
+          const gr2 = createBillBoard({
+            name: "Billboard Group 9 (Zeta Office)",
+            billboard: { mesh: bb.clone(), pos: [0, 200, 0], rot: [0, 0, 0] },
+            plane: { scale: [109, 107, 0], pos: [0, 167.5, 8] },
+            css: { scale: .17, id: "bill9" }, scaleFactor: .5
+          })
+
+          l(gr2)
+        })
+      })
+      
     }
     
     mgr.onError = url => {
@@ -549,10 +601,10 @@ export class ThreeSceneComponent implements OnInit {
       addBg()
       // City
       // addCity()
-      // Billboard
-      // addBillboards()
-      // Billboards roadside
-      addBillboards2()
+      // Building ads
+      // addBuildingAds()
+      // Billboards
+      addBillboards()
     })()
   }
   showLocation(location){
